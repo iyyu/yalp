f14ab8be87843329525cb2ba8e2041e32294342b
Emp-Norton <jamesjelenko@gmail.com>
2018-01-18 15:19:23 -0800

    profile page now renders names from actual google API results.

--- database/index.js
+++ database/index.js
@@ -15,7 +15,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'toor',
     database: 'yalp'
   })
 }
