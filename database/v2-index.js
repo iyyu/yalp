fcc67fefa822cae57786b2ebd95ce9ba65ed2831
Iris Yu <iyucomms@gmail.com>
2018-01-18 12:08:45 -0800

    Add dummy data and new deals table

--- database/v2-index.js
+++ database/v2-index.js
@@ -1,1 +1,10 @@
 // for v2 team to write our own database query functions
+const connection = require('./index.js');
+
+const addDeals = (lat, lng, cb) => {
+  
+  const sql = `SELECT * FROM listings WHERE id=${id}`;
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (err, result, fields) => err ? reject(err) : resolve(result));
+  });
+};
